d3b57f0edc8a4d5799fdfd0362ea915c5b452501
wisyuto <99457594+wisyuto@users.noreply.github.com>
2022-02-12 21:46:23 -0600

    Update config.js
    add invoke url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // デプロイするリージョンを記載。e.g. ap-northeast-1
     },
     api: {
-        invokeUrl: '' // 今は記載不要。e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://jdo7ca2bli.execute-api.us-east-1.amazonaws.com/prod' // 今は記載不要。e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
