47ccd05c7d9528e452127c5a777b7a93a8256602
wisyuto <99457594+wisyuto@users.noreply.github.com>
2022-02-12 21:10:44 -0600

    add cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_7VyOPqOy8', // プールIDを記載。e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'v7fbsdv6queg6vcpeenoc10ql', // アプリクライアントIDを記載。e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // デプロイするリージョンを記載。e.g. ap-northeast-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // 今は記載不要。e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
